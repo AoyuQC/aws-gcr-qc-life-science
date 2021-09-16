2c355bcd45b957809d32729194a8fdcba5273c27
amliuyong <am.liuyong@qq.com>
2021-09-16 15:41:58 +0800

    add braket_install

--- protein/ligand-unfolding/batch-experiment/build.sh
+++ protein/ligand-unfolding/batch-experiment/build.sh
@@ -1,2 +1,9 @@
 
+rm -rf braket_install/ > /dev/null 2>&1
+
+mkdir braket_install/ && cd braket_install/
+aws s3 cp s3://braketnotebookcdk-notebooklccs3bucketb3089b50-1w1epzvg1km1k/notebook/braket-notebook-lcc.zip .
+unzip braket-notebook-lcc.zip
+rm braket-notebook-lcc.zip
+
 docker build -t qc-batch-experiment .
