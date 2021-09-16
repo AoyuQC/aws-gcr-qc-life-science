debc61a7213c3d6ba4317bda936486bdadc28a19
amliuyong <am.liuyong@qq.com>
2021-09-16 16:57:21 +0800

    add arg for M and device-arn

--- protein/ligand-unfolding/batch-experiment/batch-experiment.py
+++ protein/ligand-unfolding/batch-experiment/batch-experiment.py
@@ -1,21 +1,40 @@
-import numpy as np
+import argparse
 import logging
-from braket.ocean_plugin import BraketSampler, BraketDWaveSampler
-import dimod
-from dwave.system.composites import EmbeddingComposite
+
 import boto3
-
-logging.getLogger().setLevel(logging.INFO)
-
+import dimod
+import numpy as np
+from braket.ocean_plugin import BraketDWaveSampler
+from dwave.system.composites import EmbeddingComposite
 # experiment for biopandas
 from biopandas.pdb import PandasPdb
+
+
+logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%Y-%m-%d:%H:%M:%S',
+                        level=logging.INFO)
+
+
+DEFAULT_DEVICE_ARN = 'arn:aws:braket:::device/qpu/d-wave/Advantage_system1'
+DEFAULT_M = 4
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--M', type=str, default=DEFAULT_M)
+parser.add_argument('--device-arn', type=str, default=DEFAULT_DEVICE_ARN)
+args, _ = parser.parse_known_args()
+logging.info("args:", args)
+
+M = args.M
+device_arn = args.device_arn
+
 
 def residue_func(row):
     return row['residue_name'] + '_' + str(row['residue_number'])
 
+
 ligand = PandasPdb().read_pdb('./peptide.pdb')
 ligand_pddf = ligand.df['ATOM']
-ligand_pddf['residue_name'] = ligand_pddf.apply(lambda row: residue_func(row),axis=1)
+ligand_pddf['residue_name'] = ligand_pddf.apply(lambda row: residue_func(row), axis=1)
 ligand_pddf.head()
 
 # parameters for experiments
@@ -24,7 +43,7 @@ n_c = 1000
 # num shots for quantum
 n_q = 1000
 # M = len(sort_torsion_group)
-M = 4
+#M = 4
 # resolution = 360/8
 D = 8
 
@@ -32,40 +51,46 @@ D = 8
 def sub_list(l1, l2):
     result = []
     for n in range(len(l1)):
-        result.append(l1[n]-l2[n])
+        result.append(l1[n] - l2[n])
     return result
+
 
 def add_list(l1, l2):
     result = []
     for n in range(len(l1)):
-        result.append(l1[n]+l2[n])
+        result.append(l1[n] + l2[n])
     return result
+
 
 def rount_list(l):
     result = []
     for n in l:
         result.append(round(n, 4))
     return result
+
+
 """
     Return a point rotated about an arbitrary axis in 3D.
     Positive angles are counter-clockwise looking down the axis toward the origin.
     The coordinate system is assumed to be right-hand.
     Arguments: 'axis point 1', 'axis point 2', 'point to be rotated', 'angle of rotation (in radians)' >> 'new point' 
 """
+
+
 def PointRotate3D(p1, p2, p0, theta):
-#     from point import Point
+    #     from point import Point
     from math import cos, sin, sqrt
 
     # Translate so axis is at origin    
     p = sub_list(p0, p1)
     # Initialize point q
-    q = [0.0,0.0,0.0]
+    q = [0.0, 0.0, 0.0]
     N = sub_list(p2, p1)
-#     Nm = sqrt(N.x**2 + N.y**2 + N.z**2)
-    Nm = sqrt(N[0]**2 + N[1]**2 + N[2]**2)
-    
+    #     Nm = sqrt(N.x**2 + N.y**2 + N.z**2)
+    Nm = sqrt(N[0] ** 2 + N[1] ** 2 + N[2] ** 2)
+
     # Rotation axis unit vector
-    n = [N[0]/Nm, N[1]/Nm, N[2]/Nm]
+    n = [N[0] / Nm, N[1] / Nm, N[2] / Nm]
 
     # Matrix common factors     
     c = cos(theta)
@@ -76,27 +101,28 @@ def PointRotate3D(p1, p2, p0, theta):
     Z = n[2]
 
     # Matrix 'M'
-    d11 = t*X**2 + c
-    d12 = t*X*Y - s*Z
-    d13 = t*X*Z + s*Y
-    d21 = t*X*Y + s*Z
-    d22 = t*Y**2 + c
-    d23 = t*Y*Z - s*X
-    d31 = t*X*Z - s*Y
-    d32 = t*Y*Z + s*X
-    d33 = t*Z**2 + c
+    d11 = t * X ** 2 + c
+    d12 = t * X * Y - s * Z
+    d13 = t * X * Z + s * Y
+    d21 = t * X * Y + s * Z
+    d22 = t * Y ** 2 + c
+    d23 = t * Y * Z - s * X
+    d31 = t * X * Z - s * Y
+    d32 = t * Y * Z + s * X
+    d33 = t * Z ** 2 + c
 
     #            |p.x|
     # Matrix 'M'*|p.y|
     #            |p.z|
-    q[0] = d11*p[0] + d12*p[1] + d13*p[2]
-    q[1] = d21*p[0] + d22*p[1] + d23*p[2]
-    q[2] = d31*p[0] + d32*p[1] + d33*p[2]
+    q[0] = d11 * p[0] + d12 * p[1] + d13 * p[2]
+    q[1] = d21 * p[0] + d22 * p[1] + d23 * p[2]
+    q[2] = d31 * p[0] + d32 * p[1] + d33 * p[2]
 
     # Translate axis and rotated point back to original location
     sum_p = add_list(q, p1)
     final_p = rount_list(sum_p)
     return final_p
+
 
 # stats about num for each atom group
 # https://stackoverflow.com/questions/46498118/pandas-use-one-column-for-groupby-and-get-stats-for-multiple-other-columns
@@ -116,7 +142,7 @@ for index, row in ligand_number_pddf.iterrows():
     atom_number_min[row['atom_number_min']] = row['residue_name']
     atom_number_max[row['atom_number_max']] = row['residue_name']
 
-for k,v in atom_number_min.items():
+for k, v in atom_number_min.items():
     max_candidate = k - 1
     if max_candidate in atom_number_max.keys():
         torsion_name = [atom_number_max[max_candidate], v]
@@ -128,33 +154,35 @@ sort_torsion_group
 # construct fragment group
 fragment_group = {}
 
+
 def get_pts(name, ligand):
     atom_pddf = ligand.loc[(ligand['residue_name'] == name)]
-    
+
     max_idx = max(atom_pddf['atom_number'])
     pts_len = len(atom_pddf)
     min_idx = max_idx - pts_len + 1
-    
+
     max_pts = []
-    max_pts.append(float(atom_pddf[atom_pddf['atom_number']==max_idx]['x_coord'].values[0]))
-    max_pts.append(float(atom_pddf[atom_pddf['atom_number']==max_idx]['y_coord'].values[0]))
-    max_pts.append(float(atom_pddf[atom_pddf['atom_number']==max_idx]['z_coord'].values[0]))
+    max_pts.append(float(atom_pddf[atom_pddf['atom_number'] == max_idx]['x_coord'].values[0]))
+    max_pts.append(float(atom_pddf[atom_pddf['atom_number'] == max_idx]['y_coord'].values[0]))
+    max_pts.append(float(atom_pddf[atom_pddf['atom_number'] == max_idx]['z_coord'].values[0]))
 
     min_pts = []
-    min_pts.append(float(atom_pddf[atom_pddf['atom_number']==min_idx]['x_coord'].values[0]))
-    min_pts.append(float(atom_pddf[atom_pddf['atom_number']==min_idx]['y_coord'].values[0]))
-    min_pts.append(float(atom_pddf[atom_pddf['atom_number']==min_idx]['z_coord'].values[0]))
+    min_pts.append(float(atom_pddf[atom_pddf['atom_number'] == min_idx]['x_coord'].values[0]))
+    min_pts.append(float(atom_pddf[atom_pddf['atom_number'] == min_idx]['y_coord'].values[0]))
+    min_pts.append(float(atom_pddf[atom_pddf['atom_number'] == min_idx]['z_coord'].values[0]))
 
     mid_pts = []
 
-    for idx in range(min_idx+1, max_idx):
+    for idx in range(min_idx + 1, max_idx):
         current_pts = []
-        current_pts.append(float(atom_pddf[atom_pddf['atom_number']==idx]['x_coord'].values[0]))
-        current_pts.append(float(atom_pddf[atom_pddf['atom_number']==idx]['y_coord'].values[0]))
-        current_pts.append(float(atom_pddf[atom_pddf['atom_number']==idx]['z_coord'].values[0]))
+        current_pts.append(float(atom_pddf[atom_pddf['atom_number'] == idx]['x_coord'].values[0]))
+        current_pts.append(float(atom_pddf[atom_pddf['atom_number'] == idx]['y_coord'].values[0]))
+        current_pts.append(float(atom_pddf[atom_pddf['atom_number'] == idx]['z_coord'].values[0]))
         mid_pts.append(current_pts)
-        
-    return min_idx, max_idx, range(min_idx+1, max_idx), min_pts, max_pts, mid_pts
+
+    return min_idx, max_idx, range(min_idx + 1, max_idx), min_pts, max_pts, mid_pts
+
 
 for tor in sort_torsion_group:
     first_red_idx = tor[0]
@@ -170,7 +198,7 @@ for tor in sort_torsion_group:
         initial_value['mid_idx'] = mid_idx
         initial_value['max_idx'] = [max_idx]
         fragment_group[first_red_name] = initial_value
-        
+
     if second_red_name not in fragment_group.keys():
         min_idx, max_idx, mid_idx, min_pts, max_pts, mid_pts = get_pts(second_red_name, ligand_pddf)
         initial_value = {}
@@ -182,11 +210,14 @@ for tor in sort_torsion_group:
         initial_value['max_idx'] = [max_idx]
         fragment_group[second_red_name] = initial_value
 
+
 def get_idx(var):
-    return int(var.split('_')[1])-1
+    return int(var.split('_')[1]) - 1
+
 
 def get_theta(var, theta_option):
-    return theta_option[int(var.split('_')[-1])-1]
+    return theta_option[int(var.split('_')[-1]) - 1]
+
 
 def get_current_pts(fragment_name, fragment_group, pts_name, pts_dict):
     target_pts = []
@@ -194,10 +225,11 @@ def get_current_pts(fragment_name, fragment_group, pts_name, pts_dict):
         target_pts = fragment_group[fragment_name][pts_name]
     else:
         if pts_name not in pts_dict[fragment_name].keys():
-            target_pts = fragment_group[fragment_name][pts_name]    
+            target_pts = fragment_group[fragment_name][pts_name]
         else:
             target_pts = pts_dict[fragment_name][pts_name]
     return target_pts
+
 
 def update_pts(start_pts, end_pts, pts_list, rotate_theta):
     rotate_list = []
@@ -206,29 +238,32 @@ def update_pts(start_pts, end_pts, pts_list, rotate_theta):
     logging.debug("pts_list {}".format(pts_list))
     pi = 3.1415926
     for pt in pts_list:
-        rotate_list.append(PointRotate3D(start_pts[0], end_pts[0], pt, rotate_theta/180*pi))
+        rotate_list.append(PointRotate3D(start_pts[0], end_pts[0], pt, rotate_theta / 180 * pi))
     return rotate_list
+
 
 def update_pts_dict(target_name, pts_name, pts_dict, rotate_pts):
     if target_name not in pts_dict.keys():
         pts_dict[target_name] = {}
     pts_dict[target_name][pts_name] = rotate_pts
-    
+
+
 def calc_distance_between_pts(pts1, pts2):
     pts1_middle = np.array(tuple(list(np.mean(np.array(pts1), axis=0))))
     pts2_middle = np.array(tuple(list(np.mean(np.array(pts2), axis=0))))
-    return np.linalg.norm(pts1_middle-pts2_middle)
+    return np.linalg.norm(pts1_middle - pts2_middle)
+
 
 def calc_distance_func(rotate_values, sort_torsion_group, fragment_group, theta_option):
     # save temp results for pts
     temp_pts_dict = {}
-    
+
     tor_base_idx = get_idx(rotate_values[0])
     tor_len = len(rotate_values)
-    tor_target_idx = get_idx(rotate_values[tor_len-1])
-    
+    tor_target_idx = get_idx(rotate_values[tor_len - 1])
+
     current_start_idx = []
-    
+
     for left_idx in range(tor_len):
         # get rotate theta
         tor_start_value = rotate_values[left_idx]
@@ -241,14 +276,16 @@ def calc_distance_func(rotate_values, sort_torsion_group, fragment_group, theta_
 
         end_name = tor_start[1][1]
         end_pts = get_current_pts(end_name, fragment_group, 'min_pts', temp_pts_dict)
-        logging.debug("current tor {} with base_idx {} rotate_theta {}".format(tor_start_value, tor_start_idx, rotate_theta))
-        
-        for right_idx in range(left_idx,tor_len):
+        logging.debug(
+            "current tor {} with base_idx {} rotate_theta {}".format(tor_start_value, tor_start_idx, rotate_theta))
+
+        for right_idx in range(left_idx, tor_len):
             tor_end_value = rotate_values[right_idx]
             tor_end_idx = get_idx(tor_end_value)
             tor_end = sort_torsion_group[tor_end_idx]
-            logging.debug("update tor  {}, tor_end_id is {}, tor_target_idx is {}".format(tor_end, tor_end_idx, tor_end_idx))
-#             only update min/max pts
+            logging.debug(
+                "update tor  {}, tor_end_id is {}, tor_target_idx is {}".format(tor_end, tor_end_idx, tor_end_idx))
+            #             only update min/max pts
 
             # rotate target min/max pts
             end_name = tor_end[1][1]
@@ -261,51 +298,53 @@ def calc_distance_func(rotate_values, sort_torsion_group, fragment_group, theta_
 
             update_pts_dict(end_name, 'min_pts', temp_pts_dict, end_min_rotate_pts)
             update_pts_dict(end_name, 'max_pts', temp_pts_dict, end_max_rotate_pts)
-            
-            
+
             if int(tor_end_idx) == int(tor_target_idx):
                 # update all the pts
                 logging.debug("update all pts!!!!!!!!")
                 mid_pts = get_current_pts(end_name, fragment_group, 'mid_pts', temp_pts_dict)
                 end_mid_rotate_pts = update_pts(start_pts, end_pts, mid_pts, rotate_theta)
-                
-#                 fragment_group[target_pts_name]['mid_pts'] = target_mid_rotate_pts
+
+                #                 fragment_group[target_pts_name]['mid_pts'] = target_mid_rotate_pts
                 update_pts_dict(end_name, 'mid_pts', temp_pts_dict, end_mid_rotate_pts)
-            
+
             logging.debug("#########pts_dict {}".format(temp_pts_dict))
-            
+
     base_name = sort_torsion_group[tor_base_idx][1][0]
     target_name = sort_torsion_group[tor_target_idx][1][1]
-    base_pts = fragment_group[base_name]['mid_pts'] + fragment_group[base_name]['min_pts'] + fragment_group[base_name]['max_pts']
-    target_pts = temp_pts_dict[target_name]['mid_pts'] + temp_pts_dict[target_name]['min_pts'] + temp_pts_dict[target_name]['max_pts']
+    base_pts = fragment_group[base_name]['mid_pts'] + fragment_group[base_name]['min_pts'] + fragment_group[base_name][
+        'max_pts']
+    target_pts = temp_pts_dict[target_name]['mid_pts'] + temp_pts_dict[target_name]['min_pts'] + \
+                 temp_pts_dict[target_name]['max_pts']
     logging.debug("base pts {}".format(base_pts))
     logging.debug("target pts {} ".format(target_pts))
     distance = calc_distance_between_pts(base_pts, target_pts)
-    
+
     return distance
+
 
 # Torsion（m）；Options for score（d）
 # Variables （v = m * d）
 # initial variable string
-theta_option = [x * 360/D for x in range(D)]
+theta_option = [x * 360 / D for x in range(D)]
 var = {}
 
 for m in range(M):
     x_d = {}
     for d in range(D):
-        x_d[d+1] = 'x_{}_{}'.format(m+1, d+1)
-    var[m+1] = x_d 
+        x_d[d + 1] = 'x_{}_{}'.format(m + 1, d + 1)
+    var[m + 1] = x_d
 
 # initial constraint 
 A = 10000
 hubo = {}
 for m in range(M):
     for d1 in range(D):
-        var_1 = var[m+1][d1+1]
+        var_1 = var[m + 1][d1 + 1]
         for d2 in range(D):
-            var_2 = var[m+1][d2+1]
+            var_2 = var[m + 1][d2 + 1]
             if (var_2, var_1) in hubo.keys():
-                hubo[(var_2,var_1)] = hubo[(var_2, var_1)] + A
+                hubo[(var_2, var_1)] = hubo[(var_2, var_1)] + A
             elif var_1 == var_2:
                 hubo[(var_1,)] = -A
             else:
@@ -314,29 +353,32 @@ for m in range(M):
 # initial distance function: D_a_b
 hubo_distances = {}
 # fragment num
-F = M+1
+F = M + 1
+
 
 def update_hubo(tor_group, up_list):
     if len(tor_group) == 1:
-#         print(tor_group)
+        #         logging.info(tor_group)
         for d in range(D):
-            final_list = up_list + [var[tor_group[0]][d+1]]
+            final_list = up_list + [var[tor_group[0]][d + 1]]
             # distance
-            hubo_distances[tuple(final_list)] = -calc_distance_func(tuple(final_list), sort_torsion_group, fragment_group, theta_option)
+            hubo_distances[tuple(final_list)] = -calc_distance_func(tuple(final_list), sort_torsion_group,
+                                                                    fragment_group, theta_option)
     else:
         for d in range(D):
-            final_list = up_list + [var[tor_group[0]][d+1]]
+            final_list = up_list + [var[tor_group[0]][d + 1]]
             update_hubo(tor_group[1:], final_list)
 
+
 for a in range(F):
-    for b in range(a+1,F):
-        logging.info("fragment {}-{}".format(a,b))
+    for b in range(a + 1, F):
+        logging.info("fragment {}-{}".format(a, b))
         start_pos = a
-        end_pos = b-a
+        end_pos = b - a
         torsion_group = []
-        while(end_pos > 0):
+        while (end_pos > 0):
             torsion_pos = start_pos + 1
-#             print("torsion {}".format(torsion_pos))
+            #             logging.info("torsion {}".format(torsion_pos))
             start_pos = start_pos + 1
             end_pos = end_pos - 1
             torsion_group.append(torsion_pos)
@@ -361,14 +403,13 @@ sampler = dimod.SimulatedAnnealingSampler()
 response = sampler.sample(qubo, num_reads=num_shots)
 
 # print results
-print(response)
+logging.info(response)
 
 end = time.time()
 
-t_sum_classic = (end-start)/60
+t_sum_classic = (end - start) / 60
 
-print("elasped time for sa of {} shots: {} min".format(num_shots, t_sum_classic))
-
+logging.info("elasped time for sa of {} shots: {} min".format(num_shots, t_sum_classic))
 
 response_aggregate = response.aggregate()
 
@@ -380,11 +421,11 @@ account_id = boto3.client('sts').get_caller_identity().get('Account')
 session = boto3.session.Session()
 aws_region = session.region_name
 
-print("account_id:{}, aws_region:{}".format(account_id, aws_region) )
+logging.info("account_id:{}, aws_region:{}".format(account_id, aws_region))
 
-my_bucket = "amazon-braket-gcrqc-{}-{}".format(account_id, aws_region) # the name of the bucket
-my_prefix = "annealer-experiment" # the name of the folder in the bucket
-print("my_bucket:", my_bucket)
+my_bucket = "amazon-braket-gcrqc-{}-{}".format(account_id, aws_region)  # the name of the bucket
+my_prefix = "annealer-experiment"  # the name of the folder in the bucket
+logging.info("my_bucket:", my_bucket)
 
 s3_folder = (my_bucket, my_prefix)
 
@@ -394,40 +435,35 @@ num_reads = n_q
 start = time.time()
 # run BQM: solve with the D-Wave 2000Q device
 # sampler = BraketDWaveSampler(s3_folder,'arn:aws:braket:::device/qpu/d-wave/DW_2000Q_6')
-sampler = BraketDWaveSampler(s3_folder,'arn:aws:braket:::device/qpu/d-wave/Advantage_system1')
+sampler = BraketDWaveSampler(s3_folder, device_arn)
 end = time.time()
-t1 = (end-start)/60
-print("elasped time for init sampler {} min".format(t1))
+t1 = (end - start) / 60
+logging.info("elasped time for init sampler {} min".format(t1))
 
 start = time.time()
 sampler = EmbeddingComposite(sampler)
 end = time.time()
-t2 = (end-start)/60
-print("elasped time for embedding generation {} min".format(t2))
+t2 = (end - start) / 60
+logging.info("elasped time for embedding generation {} min".format(t2))
 
 start = time.time()
 sampleset = sampler.sample(qubo, num_reads=num_reads)
 end = time.time()
-t3 = (end-start)/60
-print("elasped time for d wave of {} shots: {} min".format(num_shots, t3))
+t3 = (end - start) / 60
+logging.info("elasped time for d wave of {} shots: {} min".format(num_shots, t3))
 
 t_sum_quantum = t1 + t2 + t3
 
 ## aggregate solution: 
-#sampleset = sampleset.aggregate()
-#pddf_sample_result = sampleset.to_pandas_dataframe()
-#valid_name = []
-#for m in range(M):
+# sampleset = sampleset.aggregate()
+# pddf_sample_result = sampleset.to_pandas_dataframe()
+# valid_name = []
+# for m in range(M):
 #    for d in range(D):
 #        valid_name.append('x_{}_{}'.format(m+1, d+1))
 #
 ## print solution
-#print(sampleset)
+# logging.info(sampleset)
 
-print("the sum time for sa {}".format(t_sum_classic))
-print("the sum time for qa {}".format(t_sum_quantum))
-
-
-
-
-
+logging.info("the sum time for sa {}".format(t_sum_classic))
+logging.info("the sum time for qa {}".format(t_sum_quantum))
