674383aed74bf364eb85b81596d4735723ec646e
amliuyong <am.liuyong@qq.com>
2021-09-18 20:12:42 +0800

    add release code

--- cdk/synth_cf_s3.sh
+++ cdk/synth_cf_s3.sh
@@ -2,14 +2,20 @@
 
 ReleaseVersion=$1
 
-cdk --profile rsops synth > gcr-sol-qc.yaml
-aws --profile rsops s3 cp ./gcr-sol-qc.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/qc/ --acl public-read
+AWS_CMD="aws"
+CDK_CMD='cdk'
+if [[ -n $PROFILE ]];then
+  AWS_CMD="aws --profile $PROFILE"
+  CDK_CMD="cdk --profile $PROFILE"
+fi
+
+$CDK_CMD synth > gcr-sol-qc.yaml
+$AWS_CMD s3 cp ./gcr-sol-qc.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/qc/ --acl public-read
 
 if [[ -n $ReleaseVersion ]];then
-  aws --profile rsops s3 cp ./gcr-sol-qc.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/qc/gcr-sol-qc-$ReleaseVersion.yaml --acl public-read
+  $AWS_CMD s3 cp ./gcr-sol-qc.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/qc/gcr-sol-qc-$ReleaseVersion.yaml --acl public-read
   echo ""
   echo "https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/qc/gcr-sol-qc-$ReleaseVersion.yaml"
-
 fi
 
 echo ""
--- protein/ligand-unfolding/batch-experiment/cf/cf_to_s3.sh
+++ protein/ligand-unfolding/batch-experiment/cf/cf_to_s3.sh
@@ -2,10 +2,15 @@
 
 ReleaseVersion=$1
 
-aws --profile rsops s3 cp ./qc-batch.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/qc/ --acl public-read
+AWS_CMD="aws"
+if [[ -n $PROFILE ]];then
+  AWS_CMD="aws --profile $PROFILE"
+fi
+
+$AWS_CMD s3 cp ./qc-batch.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/qc/ --acl public-read
 
 if [[ -n $ReleaseVersion ]];then
-  aws --profile rsops s3 cp ./qc-batch.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/qc/qc-batch-$ReleaseVersion.yaml --acl public-read
+  $AWS_CMD s3 cp ./qc-batch.yaml s3://aws-gcr-rs-sol-workshop-ap-northeast-1-common/qc/qc-batch-$ReleaseVersion.yaml --acl public-read
   echo ""
   echo "https://aws-gcr-rs-sol-workshop-ap-northeast-1-common.s3.ap-northeast-1.amazonaws.com/qc/gcr-sol-qc-$ReleaseVersion.yaml"
 
